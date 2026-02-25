ee2ad5ef94d619593c701aa87d88d2ac8bead93c
BenjaminWang123 <benj91zc@uw.edu>
2026-02-25 14:43:31 -0800

    Fix CORS for GitHub Pages

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Define CORS options
 let corsOptions = {
-    origin: ['https://localhost', 'https://www.github.io', 'https://jakobzhao.github.io'],
+    origin: ['https://localhost:3000', 'https://benjaminwang123.github.io'],
     optionsSuccessStatus: 200
 }
 
